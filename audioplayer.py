417b40532358ca82f12040f9cf532191bc3e9683
Jenny Xu <jennycxu@mit.edu>
2018-10-21 03:36:21 -0400

    add all audio remove mp3

--- audioplayer.py
+++ audioplayer.py
@@ -18,7 +18,7 @@ class MainWidget(BaseWidget) :
 
         self.audio.set_generator(self.mixer)
 
-        keys = ["shelter","workit","harder","doit","stronger","drop","drop2","drop3","drop4","drop5"]
+        keys = ["shelter","workit","harder","doit","stronger","drop","drop2","drop3","drop4","drop5","getdown","getdownbuild","getup","overtime","again","build"]
         self.songs = {}
         for key in keys:
             self.songs[key] = WaveFile("data/" + key + ".wav")
@@ -33,6 +33,12 @@ class MainWidget(BaseWidget) :
         self.drop_gen3 = WaveGenerator(self.songs['drop3'],True)
         self.drop_gen4 = WaveGenerator(self.songs['drop4'],True)
         self.drop_gen5 = WaveGenerator(self.songs['drop5'],True)
+        self.getdown_gen = WaveGenerator(self.songs['getdown'])
+        self.getdownbuild_gen = WaveGenerator(self.songs['getdownbuild'],True)
+        self.getup_gen = WaveGenerator(self.songs['getup'])
+        self.overtime_gen = WaveGenerator(self.songs['overtime'])
+        self.again_gen = WaveGenerator(self.songs['again'])
+        self.build_gen = WaveGenerator(self.songs['build'],True)
 
         self.gens = []
         self.gens.append(self.shelter_gen)
@@ -45,6 +51,12 @@ class MainWidget(BaseWidget) :
         self.gens.append(self.drop_gen3)
         self.gens.append(self.drop_gen4)
         self.gens.append(self.drop_gen5)
+        self.gens.append(self.getdown_gen)
+        self.gens.append(self.getdownbuild_gen)
+        self.gens.append(self.getup_gen)
+        self.gens.append(self.overtime_gen)
+        self.gens.append(self.again_gen)
+        self.gens.append(self.build_gen)
 
         self.launchpad = {}
         for i in range(len(self.gens)): 
@@ -62,7 +74,7 @@ class MainWidget(BaseWidget) :
         self.launchpad[key] = WaveGenerator(self.songs[key])
 
     def play_audio(self, key):
-        print("PLAY THIS AUDIO " , key, " and exists: ",key in self.launchpad.keys())
+        #print("PLAY THIS AUDIO " , key, " and exists: ",key in self.launchpad.keys())
         if(key in self.launchpad.keys()):
             if(self.launchpad[key].loop):
                 self.launchpad[key].play_toggle()
@@ -101,6 +113,18 @@ class MainWidget(BaseWidget) :
             self.play_audio("drop4")
         elif(keycode[1] == 'g'):
             self.play_audio("drop5")
+        elif(keycode[1] == 'h'):
+            self.play_audio("getdown")
+        elif(keycode[1] == 'j'):
+            self.play_audio("getdownbuild")
+        elif(keycode[1] == 'k'):
+            self.play_audio("getup")
+        elif(keycode[1] == 'l'):
+            self.play_audio("build")
+        elif(keycode[1] == 'z'):
+            self.play_audio("overtime")
+        elif(keycode[1] == 'x'):
+            self.play_audio("again")
             
 
 
